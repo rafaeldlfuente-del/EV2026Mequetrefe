2ea643ccb58c75f288b0a9b2c14a5011cc40813b
rafaeldlfuente-del <rafael.dlfuente@gmail.com>
2026-05-18 13:24:37 +0200

    feat: Update service worker and version
    Updates the service worker cache name and logic to ensure a fresh cache is used for new versions. Also increments the displayed version in the UI.

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mequetrefe-calc-v1';
+const CACHE_NAME = 'mequetrefe-calc-v3';
 const ASSETS = [
   './',
   './index.html',
@@ -7,8 +7,19 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
   );
 });
 
